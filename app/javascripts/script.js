4ec9133da83215d2c41a0f9ce71ea5c7525653d3
Grav Gupta <garvg7904@gmail.com>
2022-02-12 04:15:03 +0530

    navigation

---
+++ app/javascripts/script.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+//the basic f sass is to write easy and efficient css code nd we need just to compile it so it can be processed by our sass and turn into a ral output file
+function hablo () {
+  document.querySelectorAll(".hammenu")[0].addEventListener("click", function () {
+  //document.addClass("menu");
+  document.querySelector(".hammenu").classList.add("hammenu-none");
+  document.querySelector("svg").classList.add("close");
+  document.querySelector(".menU").classList.add("menu");
+  sablo();
+});
+}
+
+function sablo () {
+  document.querySelector("svg").addEventListener("click", function () {
+  //document.addClass("menu");
+  document.querySelector(".hammenu").classList.remove("hammenu-none");
+  document.querySelector("svg").classList.remove("close");
+  document.querySelector(".menU").classList.remove("menu");
+
+});
+}
+
+window.onload=hablo();
